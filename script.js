4820aced3da2d2f26cab0d0b764211cd76fb1d59
Juliet Bunde <jbunde@OAKAR.local>
2026-03-12 17:48:35 +0300

    script

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function checkEligibility(){
     let age = document.getElementById("age").value;
     let monthlyIncome = document.getElementById("monthlyincome").value;
-    let employmentTypes = document.getElementById("employmentTypes");
+    let employmentTypes = document.getElementById("employmentTypes").value;
     let creditScore = document.getElementById("creditScore").value;
+
+//convert value to number
+age = parseFloat(age);
+monthlyIncome = parseFloat(monthlyIncome);
+creditScore = parseFloat(creditScore);
+
+//get selected employment type
+    let selectedEmploymentType = employmentTypes.options(employmentTypes.selectedIndex).value;
+
+    //check eligibility criteria
+    
 }
